093b922a64b594a8a3ab821f1926816e9424e8e8
THOMAS SANTOS DE SOUZA <thomas.ssouza1@senacsp.edu.br>
2026-02-27 08:45:09 -0300

    javascript

--- 07-condicionaiscomfront/js/script.js
+++ 07-condicionaiscomfront/js/script.js
@@ -1,7 +1,23 @@
-//comando DOM e evento de click
+//comando DOM e evento de click 
 document.getElementById("btnVerificar").addEventListener("click", verificar);
 
 //Função
 function verificar(){
+    const input = document.getElementById("numero").value;
+    const resultado = document.getElementById("resultado");
 
+    const numero = Number(input);
+
+     if (numero > 10) {
+        resultado.innerHTML = "O número é maior que 10.";
+        resultado.style.color = "green";
+    } 
+    else if (numero === 10) {
+        resultado.innerHTML = "O número é igual a 10.";
+        resultado.style.color = "orange";
+    } 
+    else {
+        resultado.innerHTML = "O número é menor que 10.";
+        resultado.style.color = "blue";
+    }
 }
