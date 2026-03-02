2380f958af87c456ce90a9328b7668338406f357
THOMAS SANTOS DE SOUZA <thomas.ssouza1@senacsp.edu.br>
2026-03-02 11:30:31 -0300

    switch e loops

--- 06-condicionais/script.js
+++ 06-condicionais/script.js
@@ -1,17 +1,17 @@
 //if (se condição for verdadeira)
 const numerico = 10;
 
-if(numerico > 8){
+if (numerico > 8) {
     console.log("O número informado é maior que 8");
 }
 
 const nomealuno = "Juliano";
 
-if(nomealuno === "Juliano"){
+if (nomealuno === "Juliano") {
     console.log("Olá Juliano");
 }
 
-if(nomealuno === "Maria"){
+if (nomealuno === "Maria") {
     console.log("Olá Maria"); //perceba que esse bloco foi ignorado
 }
 
@@ -19,25 +19,25 @@ if(nomealuno === "Maria"){
 //else (condição se falsa)
 const logado = false;
 
-if(logado){
+if (logado) {
     console.log("Está logado!")
 }
-else{
+else {
     console.log("Não está logado!");
 }
 
 const altura = 1.70;
 
-if(altura > 1.80){
+if (altura > 1.80) {
     console.log("Essa pessoa tem mais de 1,80 de altura");
-}else{
+} else {
     console.log("Essa pessoa tem menos de 1,80 de altura");
 }
 
 const marioridade1 = "18 anos";
 const habilitado = true;
 
-if(marioridade1 >= 18 && habilitado === false){
+if (marioridade1 >= 18 && habilitado === false) {
     console.log("Não habilitada");
 } else {
     console.log("Habilitada");
@@ -47,21 +47,51 @@ if(marioridade1 >= 18 && habilitado === false){
 const cachorro = "Pastor Alemão";
 const nomedocachorro = "Zeca";
 
-if(cachorro === "Dálmata"){
+if (cachorro === "Dálmata") {
     console.log("É um dálmata!");
-}else if(nomedocachorro === "Zeca"){
-    console.log ("Se chama Zeca!");
-}else{
+} else if (nomedocachorro === "Zeca") {
+    console.log("Se chama Zeca!");
+} else {
     console.log("É um poodle");
 }
 
 
-if(1 > 2){
+if (1 > 2) {
     console.log("Teste 1 é verdadeiro");
-}else if(2 > 4){
+} else if (2 > 4) {
     console.log("Teste 2 é verdadeiro");
-}else if(4 < 7){
-    console.log ("Teste 3 é verdadeiro");
-}else{
+} else if (4 < 7) {
+    console.log("Teste 3 é verdadeiro");
+} else {
     console.log("Não temos condição verdadeira");
+}
+
+//switch
+let idade = 18;
+
+switch (idade) {
+    case 18:
+        console.log("O usuário é maior de idade");
+        break;
+    case 17:
+        console.log("O usuário é menor de idade");
+        break;
+    default:
+        console.log("Idade inválida.");
+}
+
+let opcao = "chocolate";
+
+switch (opcao) {
+    case "pizza":
+        console.log("Pizza chegando!");
+        break;
+    case "sushi":
+        console.log("Sushi fresquinho!");
+        break;
+    case "hamburguer":
+        console.log("Hambúrguer suculento!");
+        break;
+    default:
+        console.log("Não temos esse produto!");
 }
---
+++ 08-loops/script.js
@@ -0,0 +1,37 @@
+//while (enquanto)
+let x = 1;
+
+while (x <= 10) {
+    console.log(x);
+    x = x + 1;
+}
+
+let y = 10;
+
+while (y >= 5) {
+    console.log(y);
+    y--; //decremento: o mesmo que y = y - 1;
+}
+
+//do while (fazer enquanto)
+let m = 15;
+
+do {
+    console.log(`${m}`);
+    m = m + 2;
+} while (m <= 21);
+
+
+//comparando while e do while
+let n = 12;
+
+while (n <= 10) {
+    console.log(n);
+    n++; //incremento
+}
+
+//executa pelo menos 1 vez antes do loop
+do {
+    console.log(n);
+    n++;
+} while (n <= 10);
